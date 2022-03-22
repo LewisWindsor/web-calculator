3ef06b4a2c93f054c24e0e99757f59a3cc4e6205
James Lewis <LewisWindsor10@yahoo.com>
2022-03-22 17:43:01 -0400

    Completes Web Calculator Part One Answer

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function getAnswer () {
+    var paramOne = Number(document.getElementById('paramOne').value)
+    var paramTwo = Number(document.getElementById('paramTwo').value)
+    
+
+    document.getElementById('finalAnswer').innerHTML = paramOne + paramTwo
+
+} 
