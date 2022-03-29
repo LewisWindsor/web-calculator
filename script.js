61889714e4cb9357b187bf1c0cf3743404433d0d
James Lewis <LewisWindsor10@yahoo.com>
2022-03-28 21:12:28 -0400

    Finished Web Calculator Two

--- script.js
+++ script.js
@@ -3,8 +3,15 @@
 function getAnswer () {
     var paramOne = Number(document.getElementById('paramOne').value)
     var paramTwo = Number(document.getElementById('paramTwo').value)
+    var operator = document.getElementById("operation").value
 
-
-    document.getElementById('finalAnswer').innerHTML = paramOne + paramTwo
-
+    if(operator === "+"){
+        document.getElementById('finalAnswer').innerHTML = paramOne + paramTwo
+    } else if(operator === "-"){
+        document.getElementById('finalAnswer').innerHTML = paramOne - paramTwo
+    } else if(operator === "/"){
+        document.getElementById('finalAnswer').innerHTML = paramOne / paramTwo
+    } else if(operator === "*"){
+        document.getElementById('finalAnswer').innerHTML = paramOne * paramTwo
+    }
 } 
